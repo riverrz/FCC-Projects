6202d35b2b8ee2f038e75bb319435fd857808681
shivam123425 <shivam123425@gmail.com>
2018-01-12 15:28:19 +0530

    Weather - Made some minor improvements Status - Completed

--- Weather/assets/js/weather.js
+++ Weather/assets/js/weather.js
@@ -1,4 +1,5 @@
 var cel=true;
+$("button").text(String.fromCharCode(176)+"C");
 $("button").on("click", function() {
 	var value=$("span").text();
 	value=Number(value);
@@ -11,11 +12,11 @@ function convert(value) {
 	if (cel===true) {
 		var newval = Math.round((1.8)*value +32);
 		cel=false;
-		$("button").text("F");
+		$("button").text(String.fromCharCode(176)+"F");
 	}
 	else {
 		var newval = Math.round((5/9)*(value-32));
-		$("button").text("C");
+		$("button").text(String.fromCharCode(176)+"C");
 		cel=true;
 	}
 	$("span").text(String(newval));
@@ -31,11 +32,8 @@ function getLocation() {
 }
 
 function showPosition(position) {
-	console.log(position.coords.latitude);
-	console.log(position.coords.longitude);
 	$.getJSON('https://fcc-weather-api.glitch.me/api/current?lat='+String(position.coords.latitude)+'&lon='+String(position.coords.longitude), function(data) {
 		$("span").text(String(data["main"]["temp"]));
-		console.log();
 		$("#condition").text(String(data["weather"][0]["main"]));
 		$("#icon").html("<img src="+String(data["weather"][0]["icon"])+">");
 		var iconString = "wi wi-day-"+String(data["weather"][0]["description"]);
