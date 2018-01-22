d32d21daf47dffed3264763f56862ced83f0422a
shivam123425 <shivam123425@gmail.com>
2018-01-22 23:12:25 +0530

    TicTacToe - Added checkboard , added respective markers on click , STATUS-INCOMPLETE

--- Advanced Front End Development Projects/Tic Tac Toe/assets/js/tictactoe.js
+++ Advanced Front End Development Projects/Tic Tac Toe/assets/js/tictactoe.js
@@ -1,3 +1,7 @@
+var player1mark = null;
+var player2mark = null;
+var currentmark=null;
+
 $(".players").click(function() {
 	$("#firstPage").fadeOut(500, function() {
 		$("#pickPage").fadeIn(500);
@@ -9,3 +13,30 @@ $("#goBack").click(function() {
 		$("#firstPage").fadeIn(500);
 	});
 });
+
+$(".mark").click(function() {
+	player1mark=$(this).text();
+	if (player1mark==="O") {
+		player2mark="X";
+	}
+	else {
+		player2mark="O";
+	}
+	$("#pickPage").fadeOut(500, function() {
+		$("#tiles").fadeIn(500);	
+	});
+	currentmark=player1mark;
+	
+});
+
+
+$(".tile").click(function() {
+	$(this).append("<h1>"+currentmark+"</h1>");
+	if (currentmark===player1mark) {
+		currentmark=player2mark;
+	}
+	else {
+		currentmark=player1mark;
+	}
+});
+
