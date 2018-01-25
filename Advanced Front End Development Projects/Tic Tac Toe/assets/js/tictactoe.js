d01a63106ba1c9c6f801f4b80ac709b1a3118697
shivam123425 <shivam123425@gmail.com>
2018-01-25 17:57:23 +0530

    TicTacToe - Two Player Mode Completed - (Added Result checking ability)  STATUS - INCOMPLETE

--- Advanced Front End Development Projects/Tic Tac Toe/assets/js/tictactoe.js
+++ Advanced Front End Development Projects/Tic Tac Toe/assets/js/tictactoe.js
@@ -1,6 +1,13 @@
 var player1mark = null;
 var player2mark = null;
 var currentmark=null;
+var totalMoves=0;
+var allTiles = $(".tile");
+var currentPlayer="Player 1";
+
+var player1Tiles={};
+var player2Tiles={};
+
 
 $(".players").click(function() {
 	$("#firstPage").fadeOut(500, function() {
@@ -23,14 +30,31 @@ $(".mark").click(function() {
 		player2mark="O";
 	}
 	$("#pickPage").fadeOut(500, function() {
-		$("#tiles").fadeIn(500);	
+		$("#tiles").fadeIn(500, function() {
+			$("#currentPlayer").text(currentPlayer);
+		});	
 	});
 	currentmark=player1mark;
 	
 });
+$(".results").on("click",".reset",function() {
+	player1mark = null;
+	player2mark = null;
+	currentmark=null;
+	totalMoves=0;
+	currentPlayer="Player 1";
+	player1Tiles={};
+	player2Tiles={};
+	$(".results").fadeOut(500, function() {
+		$("#tiles").fadeOut(300, function() {
+			allTiles.empty();
+			$("#pickPage").fadeIn(300);
+		});
+		
+	});
+});
 
-
-$(".tile").click(function() {
+allTiles.click(function() {
 	$(this).append("<h1>"+currentmark+"</h1>");
 	if (currentmark===player1mark) {
 		currentmark=player2mark;
@@ -38,5 +62,238 @@ $(".tile").click(function() {
 	else {
 		currentmark=player1mark;
 	}
+	totalMoves+=1;
+
+
+	if (currentPlayer==="Player 1") {
+		player1Tiles[$(this).attr("id")]=1;
+		currentPlayer = "Player 2";
+	}
+	else {
+		player2Tiles[$(this).attr("id")]=1;
+		currentPlayer= "Player 1";
+	}
+	$("#currentPlayer").text(currentPlayer);
+
+	if (totalMoves===9) {
+		player1Tiles={};
+		player2Tiles={};
+		$(".results").empty();
+		$(".results").fadeIn(600,function() {
+			$(this).append("It's a Draw");	
+			$(this).fadeOut(1600, function() {
+				allTiles.empty();
+				totalMoves=0;
+				currentPlayer="Player 1";
+				currentmark=player1mark;
+				$("#currentPlayer").text(currentPlayer);
+			});
+		});
+
+	}
+	else {
+		checkResult();	
+	}
+	
 });
 
+function checkResult() {
+	var check=0;
+	if (Object.keys(player1Tiles).length<3 && Object.keys(player2Tiles).length<3) {
+		return;
+	}
+	for (var i=0;i<Object.keys(player1Tiles).length;i++) {
+		if (checkHorizontal(1,Object.keys(player1Tiles)[i]) || checkVertical(1,Object.keys(player1Tiles)[i]) || mainDiagnol(1,Object.keys(player1Tiles)[i]) || invDiagnol(1,Object.keys(player1Tiles)[i])) {
+			$(".results").empty();
+			$(".results").fadeIn(800);
+			$(".results").append("Player 1 Won :D");
+			$(".results").append('<button class="reset">Play Again?</button>');
+			return;
+
+		}
+	}
+	for (var i=0;i<Object.keys(player2Tiles).length;i++) {
+		if (checkHorizontal(2,Object.keys(player2Tiles)[i]) || checkVertical(2,Object.keys(player2Tiles)[i]) || mainDiagnol(2,Object.keys(player2Tiles)[i]) || invDiagnol(2,Object.keys(player2Tiles)[i])) {
+			$(".results").empty();
+			$(".results").fadeIn(800);
+			$(".results").append("Player 2 Won :D");
+			$(".results").append('<button class="reset">Play Again?</button>');
+			return;
+		}
+		
+		
+		
+	}
+}
+
+function checkHorizontal(y,x) {
+	if (y===1) {
+		var current=player1Tiles;	
+	}
+	else {
+		var current=player2Tiles;
+	}
+
+	x=Number(x);
+
+	// For Left Most Tiles
+
+	if (x===1 || x===4 || x===7) {
+		if (x in current && x+1 in current && x+2 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}	
+
+	
+	}
+	//For Middle Tiles
+	else if (x===2 || x===5 || x===8) {
+		if (x in current && x-1 in current && x+1 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	//For Right Most Tiles
+	else if (x===3 || x===6 || x===9) {
+		if (x in current && x-1 in current && x-2 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+function checkVertical(y,x) {
+	if (y===1) {
+		var current=player1Tiles;	
+	}
+	else {
+		var current=player2Tiles;
+	}
+
+	x=Number(x);
+
+	// For Top Most Tiles
+
+	if (x===1 || x===2 || x===3) {
+		if (x in current && x+3 in current && x+6 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}	
+
+	
+	}
+	//For Middle Tiles
+	else if (x===4 || x===5 || x===6) {
+		if (x in current && x-3 in current && x+3 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	//For Bottom Most Tiles
+	else if (x===7 || x===8 || x===9) {
+		if (x in current && x-3 in current && x-6 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+function mainDiagnol(y,x) {
+	if (y===1) {
+		var current=player1Tiles;	
+	}
+	else {
+		var current=player2Tiles;
+	}
+
+	x=Number(x);
+
+	// For Top-Left  Tile
+
+	if (x===1) {
+		if (x in current && x+4 in current && x+8 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}	
+
+	
+	}
+	//For Middle Tile
+	else if (x===5) {
+		if (x in current && x-4 in current && x+4 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	//For Bottom-Right  Tile
+	else if (x===9) {
+		if (x in current && x-4 in current && x-8 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+function invDiagnol(y,x) {
+	if (y===1) {
+		var current=player1Tiles;	
+	}
+	else {
+		var current=player2Tiles;
+	}
+
+	x=Number(x);
+
+	// For Top-Right  Tile
+
+	if (x===3) {
+		if (x in current && x+2 in current && x+4 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}	
+
+	
+	}
+	//For Middle Tile
+	else if (x===5) {
+		if (x in current && x-2 in current && x+2 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	//For Right Most Tiles
+	else if (x===7) {
+		if (x in current && x-2 in current && x-4 in current) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
