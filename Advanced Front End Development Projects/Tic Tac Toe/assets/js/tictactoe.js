0e0280812ef7a4ab45d13fa85e516819409aed40
shivam123425 <shivam123425@gmail.com>
2018-01-29 21:56:01 +0530

    TicTacToe - Removed multiple marks on a tile bug , reduced redundant arguments STATUS - FINISHED!

--- Advanced Front End Development Projects/Tic Tac Toe/assets/js/tictactoe.js
+++ Advanced Front End Development Projects/Tic Tac Toe/assets/js/tictactoe.js
@@ -79,6 +79,7 @@ allTiles.click(function() {
 		$("#player1").slideUp("slow");
 		player1Tiles[$(this).attr("id")]=1;
 		completeTiles.splice(completeTiles.indexOf($(this).attr("id")),1);
+		console.log(completeTiles);
 
 		ans=checkResult();
 
@@ -334,14 +335,14 @@ function invDiagnol(y,x) {
 
 function generatePosition() {
 	for (var i=0;i<Object.keys(player2Tiles).length;i++) {
-		if (findHorizontal(Object.keys(player2Tiles)[i],player2Tiles,player1Tiles) || findVertical(Object.keys(player2Tiles)[i],player2Tiles,player1Tiles) || findDiagnol(Object.keys(player2Tiles)[i],player2Tiles,player1Tiles) || findInvDiagnol(Object.keys(player2Tiles)[i],player2Tiles,player1Tiles)) {
-			
+		if (findHorizontal(Object.keys(player2Tiles)[i],player2Tiles) || findVertical(Object.keys(player2Tiles)[i],player2Tiles) || findDiagnol(Object.keys(player2Tiles)[i],player2Tiles) || findInvDiagnol(Object.keys(player2Tiles)[i],player2Tiles)) {
+			console.log("Mine");
 			return;
 		}
 	}
 	for (var i=0;i<Object.keys(player1Tiles).length;i++) {
-		if (findHorizontal(Object.keys(player1Tiles)[i],player1Tiles,player2Tiles) || findVertical(Object.keys(player1Tiles)[i],player1Tiles,player2Tiles) || findDiagnol(Object.keys(player1Tiles)[i],player1Tiles,player2Tiles) || findInvDiagnol(Object.keys(player1Tiles)[i],player1Tiles,player2Tiles)) {
-			
+		if (findHorizontal(Object.keys(player1Tiles)[i],player1Tiles) || findVertical(Object.keys(player1Tiles)[i],player1Tiles) || findDiagnol(Object.keys(player1Tiles)[i],player1Tiles) || findInvDiagnol(Object.keys(player1Tiles)[i],player1Tiles)) {
+			console.log("Found")
 			return;
 		}
 	}
@@ -349,128 +350,130 @@ function generatePosition() {
 	if (!(5 in player2Tiles || 5 in player1Tiles)) {
 			$("#5").append("<h1>"+currentmark+"</h1>");
 			player2Tiles["5"]=1;
+			console.log("Middle")
 			completeTiles.splice(completeTiles.indexOf("5"),1);
 			
 		}
-	else  {
+	else  { 
 			var random = Math.round(Math.random()*(completeTiles.length-1));
-			console.log(random,completeTiles);
+			
 
 			$("#"+completeTiles[random]).append("<h1>"+currentmark+"</h1>");
 			player2Tiles[completeTiles[random]]=1;
 			completeTiles.splice(random,1);
+			console.log("Random",random,completeTiles);
 	}
 	
 }
 
-function findHorizontal(x,currentlist,invcurrentlist) {
+function findHorizontal(x,currentlist) {
 	x=Number(x)
 	if (x%3===1) {
-		if (x+1 in currentlist && !(x+2 in invcurrentlist || x+2 in currentlist)) {
+		if (x+1 in currentlist && completeTiles.indexOf(String(x+2))!==-1) {
 			return putmark(String(x+2));
 		}
-		else if (x+2 in currentlist && !(x+1 in invcurrentlist || x+1 in currentlist)) {
+		else if (x+2 in currentlist && completeTiles.indexOf(String(x+1))!==-1) {
 			return putmark(String(x+1));
 		}
 	}
 	else if (x%3===2) {
-		if (x-1 in currentlist && !(x+1 in invcurrentlist || x+1 in currentlist)) {
+		if (x-1 in currentlist && completeTiles.indexOf(String(x+1))!==-1) {
 			return putmark(String(x+1));
 		}
-		else if (x+1 in currentlist && !(x-1 in invcurrentlist || x-1 in currentlist)) {
+		else if (x+1 in currentlist && completeTiles.indexOf(String(x-1))!==-1) {
 			return putmark(String(x-1));
 		}
 	}
 	else if (x%3===0) {
-		if (x-1 in currentlist && !(x-2 in invcurrentlist || x-2 in currentlist)) {
+		if (x-1 in currentlist && completeTiles.indexOf(String(x-2))!==-1) {
 			return putmark(String(x-2));
 		}
-		else if (x-2 in currentlist && !(x-1 in invcurrentlist || x-1 in currentlist)) {
+		else if (x-2 in currentlist && completeTiles.indexOf(String(x-1))!==-1) {
 			return putmark(String(x-1));
 		}
 	}
 
 }
-function findVertical(x,currentlist,invcurrentlist) {
+function findVertical(x,currentlist) {
 	x=Number(x)
 	if (x===1 || x===2 || x===3) {
-		if (x+3 in currentlist && !(x+6 in invcurrentlist || x+6 in currentlist)) {
+		if (x+3 in currentlist && completeTiles.indexOf(String(x+6))!==-1) {
 			return putmark(String(x+6));
 		}
-		else if (x+6 in currentlist &&  !(x+3 in invcurrentlist || x+3 in currentlist)) {
+		else if (x+6 in currentlist &&  completeTiles.indexOf(String(x+3))!==-1) {
 			return putmark(String(x+3));
 		}	
 	}
 	else if (x===4 || x===5 || x===6) {
-		if (x-3 in currentlist && !(x+3 in invcurrentlist || x+3 in currentlist)) {
+		if (x-3 in currentlist && completeTiles.indexOf(String(x+3))!==-1) {
 			return putmark(String(x+3));
 		}
-		else if (x+3 in currentlist && !(x-3 in invcurrentlist || x-3 in currentlist)) {
+		else if (x+3 in currentlist && completeTiles.indexOf(String(x-3))!==-1) {
 			return putmark(String(x-3));
 		}
 	}
 	else if (x===7 || x===8 || x===9) {
-		if (x-3 in currentlist && !(x+3 in invcurrentlist || x+3 in currentlist)) {
+		if (x-3 in currentlist && completeTiles.indexOf(String(x+3))!==-1) {
 			return putmark(String(x+3));
 		}
-		else if (x+3 in currentlist && !(x-3 in invcurrentlist || x-3 in currentlist)) {
+		else if (x+3 in currentlist && completeTiles.indexOf(String(x-3))!==-1) {
 			return putmark(String(x-3));
 		}
 	}
 
 }
 
-function findDiagnol(x,currentlist,invcurrentlist) {
+function findDiagnol(x,currentlist) {
 	x=Number(x)
 	if (x===1) {
-		if (5 in currentlist && !(9 in invcurrentlist || 9 in currentlist)) {
+		if (5 in currentlist && completeTiles.indexOf(String(9))!==-1) {
 			return putmark("9");
 		}
-		else if (9 in currentlist && !(5 in invcurrentlist || 5 in currentlist)) {
+		else if (9 in currentlist && completeTiles.indexOf(String(5))!==-1) {
 			return putmark("5");
 		}
 	}
 	else if (x===5) {
-		if (1 in currentlist && !(9 in invcurrentlist || 9 in currentlist)) {
+		if (1 in currentlist && completeTiles.indexOf(String(9))!==-1) {
 			return putmark("9");
 		}
-		else if (9 in currentlist && !(1 in invcurrentlist || 1 in currentlist)) {
+		else if (9 in currentlist && completeTiles.indexOf(String(1))!==-1) {
 			return putmark("1");
 		}
 	}
 	else if (x===9) {
-		if (5 in currentlist && !(1 in invcurrentlist || 1 in currentlist)) {
+		if (5 in currentlist && completeTiles.indexOf(String(1))!==-1) {
 			return putmark("1");
 		}
-		else if (1 in currentlist && !(5 in invcurrentlist || 5 in currentlist)) {
+		else if (1 in currentlist && completeTiles.indexOf(String(5))!==-1) {
 			return putmark("5");
 		}
 	}
 }
 
-function findInvDiagnol(x,currentlist,invcurrentlist) {
+function findInvDiagnol(x,currentlist) {
 	x=Number(x)
 	if (x===3) {
-		if (5 in currentlist && !(7 in invcurrentlist || 7 in currentlist)) {
+		if (5 in currentlist && completeTiles.indexOf(String(7))!==-1) {
 			return putmark("7");
 		}
-		else if (7 in currentlist && !(5 in invcurrentlist || 5 in currentlist)) {
+		else if (7 in currentlist && completeTiles.indexOf(String(5))!==-1) {
 			return putmark("5");
 		}
 	}
 	else if (x===5) {
-		if (3 in currentlist && !(7 in invcurrentlist || 7 in currentlist)) {
+		if (3 in currentlist && completeTiles.indexOf(String(7))!==-1) {
 			return putmark("7");
 		}
-		else if (7 in currentlist && !(3 in invcurrentlist || 3 in currentlist)) {
+		else if (7 in currentlist && completeTiles.indexOf(String(3))!==-1) {
 			return putmark("3");
 		}
 	}
 	else if (x===7) {
-		if (5 in currentlist && !(3 in invcurrentlist || 3 in currentlist)) {
+		if (5 in currentlist && completeTiles.indexOf(String(3))!==-1) {
 			return putmark("3");
 		}
-		else if (3 in currentlist && !(5 in invcurrentlist || 5 in currentlist)) {
+		else if (3 in currentlist && completeTiles.indexOf(String(5))!==-1) {
 			return putmark("5");
 		}
 	}
@@ -478,10 +481,8 @@ function findInvDiagnol(x,currentlist,invcurrentlist) {
 
 function putmark(x) {
 	$("#"+x).append("<h1>"+currentmark+"</h1>");
-	if (x in completeTiles) {
-		completeTiles.splice(completeTiles.indexOf(x),1);
-	}
 	player2Tiles[x]=1
-	completeTiles.indexOf(String(x),1);
+	completeTiles.splice(completeTiles.indexOf(x),1);
+	console.log(completeTiles);
 	return true;
 }
