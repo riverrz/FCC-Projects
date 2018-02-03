e47a5cc188d00b6cc684aef0153c00cfd471430c
shivam123425 <shivam123425@gmail.com>
2018-02-04 00:04:24 +0530

    Improved colour scheme , added click effect

--- Advanced Front End Development Projects/Simon's Game/assets/js/simon.js
+++ Advanced Front End Development Projects/Simon's Game/assets/js/simon.js
@@ -3,6 +3,13 @@ var count=0;
 var current=0;
 var game_length=0;
 var strict_mode=false;
+var colors =[
+	["#330000","#ff0000"],
+	["#1a3300", "#99ff33"],
+	["#000033", "#6666ff"],
+	["#666600", "#ffff1a"]
+];
+
 
 $("#start").on("click", function() {
 	generateSequence();
@@ -15,11 +22,20 @@ $("#strict").on("click", function() {
 $(".tile").on('click' , function() {
 	results($(this).attr("id"));
 });
+$(".tile").on("mousedown", function() {
+	var index = Number($(this).attr("id"));
+	$(this).css("background-color",colors[index][1]);
+});
+$(".tile").on("mouseup", function() {
+	var index= Number($(this).attr("id"));
+	$(this).css("background-color", colors[index][0]);
+});
 
 function generateSequence() {
 	var random=String(Math.round(Math.random()*3));
 	sequence.push(random);
 	console.log(sequence);
+	// displaySequence();
 };
 function results(tileClicked) {
 	if (sequence[current]===tileClicked) {
