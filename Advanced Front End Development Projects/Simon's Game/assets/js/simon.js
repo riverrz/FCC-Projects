1070a09635a4e1f05373c9e0573f4041b0047451
shivam123425 <shivam123425@gmail.com>
2018-02-04 21:51:20 +0530

    Added sounds , improved counter's visual

--- Advanced Front End Development Projects/Simon's Game/assets/js/simon.js
+++ Advanced Front End Development Projects/Simon's Game/assets/js/simon.js
@@ -1,17 +1,27 @@
 var sequence=[];
 var count=0;
 var current=0;
-var game_length=0;
+var game_length=1;
 var strict_mode=false;
+var started=false;
 var colors =[
 	["#330000","#ff0000"],
-	["#1a3300", "#99ff33"],
+	["#006600", "#66ff66"],
 	["#000033", "#6666ff"],
 	["#666600", "#ffff1a"]
+];
+var sounds = [
+	"/home/shivam/Desktop/FCC-Projects/Advanced Front End Development Projects/Simon's Game/assets/audio/sound1.mp3", 
+	"/home/shivam/Desktop/FCC-Projects/Advanced Front End Development Projects/Simon's Game/assets/audio/sound2.mp3", 
+	"/home/shivam/Desktop/FCC-Projects/Advanced Front End Development Projects/Simon's Game/assets/audio/sound3.mp3", 
+	"/home/shivam/Desktop/FCC-Projects/Advanced Front End Development Projects/Simon's Game/assets/audio/sound4.mp3", 
+	
 ];
 
 
 $("#start").on("click", function() {
+	started=true;
+	putcount();
 	generateSequence();
 });
 
@@ -20,21 +30,28 @@ $("#strict").on("click", function() {
 });
 
 $(".tile").on('click' , function() {
-	results($(this).attr("id"));
+	var index=$(this).attr("id");
+	var audio= new Audio(sounds[index]);
+	audio.play();
+	results(index);
 });
 $(".tile").on("mousedown", function() {
-	var index = Number($(this).attr("id"));
-	$(this).css("background-color",colors[index][1]);
+	if (started) {
+		var index = Number($(this).attr("id"));
+		$(this).css("background-color",colors[index][1]);	
+	}
+	
 });
 $(".tile").on("mouseup", function() {
-	var index= Number($(this).attr("id"));
-	$(this).css("background-color", colors[index][0]);
+	if (started) {
+		var index= Number($(this).attr("id"));
+		$(this).css("background-color", colors[index][0]);
+	}
 });
 
 function generateSequence() {
 	var random=String(Math.round(Math.random()*3));
 	sequence.push(random);
-	console.log(sequence);
 	displaySequence();
 };
 function results(tileClicked) {
@@ -48,6 +65,7 @@ function results(tileClicked) {
 					$(".winning_page").fadeIn("slow", function() {
 						$(".winning_page").fadeOut("slow", function() {
 							reset();
+							started=false;
 							$(".row").fadeIn("slow");
 						});
 					});
@@ -69,7 +87,7 @@ function results(tileClicked) {
 	}
 };
 function reset() {
-	game_length=0;
+	game_length=1;
 	current=0;
 	sequence=[];
 	putcount();
@@ -80,14 +98,20 @@ function putcount() {
 };
 
 function displaySequence() {
+	if (!started) {
+		return;
+	}
 	var count = 0;
 	var index;
 	var close=setInterval(function(){
 		index=sequence[count];
 		$("#"+index).css("background", colors[Number(index)][1]);
-		setTimeout(function(){$("#"+index).css("background", colors[Number(index)][0]);},650);
+		var audio= new Audio(sounds[index]);
+		audio.play();
+		setTimeout(function(){
+			$("#"+index).css("background", colors[Number(index)][0]);
+		},650);
 		count+=1;
-		console.log("yes");
 		if (count===sequence.length) {
 			clearInterval(close);
 		}
