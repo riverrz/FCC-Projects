a1f0199faa3c49d0de78dbc6da36edc5508ad690
shivam123425 <shivam123425@gmail.com>
2018-02-06 00:25:55 +0530

    Added ability to distinguish between online and offline

--- Intermediate Front End Development Projects/Twitch Tv/assets/js/twitch.js
+++ Intermediate Front End Development Projects/Twitch Tv/assets/js/twitch.js
@@ -1,6 +1,163 @@
-$.getJSON('https://wind-bow.gomix.me/twitch-api/users/ESL_SC2?callback=?', function(data) {
-	var image=data.logo;
-	$(".logo").append("<img src="+"'"+image+"'"+">");
-	console.log(data);
-	$(".name").append("<a href="+"'"+data._links.self+"' target='_blank' "+'>'+ data.display_name+"</a>");
+var data= [
+  {
+    "stream": {
+      "mature": false,
+      "status": "Greg working on Electron-Vue boilerplate w/ Akira #programming #vuejs #electron",
+      "broadcaster_language": "en",
+      "display_name": "FreeCodeCamp",
+      "game": "Creative",
+      "language": "en",
+      "_id": 79776140,
+      "name": "freecodecamp",
+      "created_at": "2015-01-14T03:36:47Z",
+      "updated_at": "2016-09-17T05:00:52Z",
+      "delay": null,
+      "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_image-d9514f2df0962329-300x300.png",
+      "banner": null,
+      "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-channel_offline_image-b8e133c78cd51cb0-1920x1080.png",
+      "background": null,
+      "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_banner-6f5e3445ff474aec-480.png",
+      "profile_banner_background_color": null,
+      "partner": false,
+      "url": "https://www.twitch.tv/freecodecamp",
+      "views": 161989,
+      "followers": 10048,
+      "_links": {
+        "self": "https://api.twitch.tv/kraken/channels/freecodecamp",
+        "follows": "https://api.twitch.tv/kraken/channels/freecodecamp/follows",
+        "commercial": "https://api.twitch.tv/kraken/channels/freecodecamp/commercial",
+        "stream_key": "https://api.twitch.tv/kraken/channels/freecodecamp/stream_key",
+        "chat": "https://api.twitch.tv/kraken/chat/freecodecamp",
+        "subscriptions": "https://api.twitch.tv/kraken/channels/freecodecamp/subscriptions",
+        "editors": "https://api.twitch.tv/kraken/channels/freecodecamp/editors",
+        "teams": "https://api.twitch.tv/kraken/channels/freecodecamp/teams",
+        "videos": "https://api.twitch.tv/kraken/channels/freecodecamp/videos"
+      }
+    },
+    "_links": {
+      "self": "https://api.twitch.tv/kraken/streams/freecodecamp",
+      "channel": "https://api.twitch.tv/kraken/channels/freecodecamp"
+    }
+  },
+  {
+    "stream": null,
+    "display_name": "OgamingSC2",
+    "default_pic":"https://static-cdn.jtvnw.net/user-default-pictures/cd618d3e-f14d-4960-b7cf-094231b04735-profile_image-70x70.jpg",
+    "_links": {
+      "self": "https://api.twitch.tv/kraken/streams/ogamingsc2",
+      "channel": "https://api.twitch.tv/kraken/channels/ogamingsc2"
+    }
+  },
+  {
+    "stream": {
+      "mature": false,
+      "status": "RERUN: StarCraft 2 - Kane vs. HuK (ZvP) - WCS Season 3 Challenger AM - Match 4",
+      "broadcaster_language": "en",
+      "display_name": "ESL_SC2",
+      "game": "StarCraft II",
+      "language": "en",
+      "_id": 30220059,
+      "name": "esl_sc2",
+      "created_at": "2012-05-02T09:59:20Z",
+      "updated_at": "2016-09-17T06:02:57Z",
+      "delay": null,
+      "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_image-d6db9488cec97125-300x300.jpeg",
+      "banner": null,
+      "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-channel_offline_image-5a8657f8393c9d85-1920x1080.jpeg",
+      "background": null,
+      "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_banner-f8295b33d1846e75-480.jpeg",
+      "profile_banner_background_color": "#050506",
+      "partner": true,
+      "url": "https://www.twitch.tv/esl_sc2",
+      "views": 60843789,
+      "followers": 135275,
+      "_links": {
+        "self": "https://api.twitch.tv/kraken/channels/esl_sc2",
+        "follows": "https://api.twitch.tv/kraken/channels/esl_sc2/follows",
+        "commercial": "https://api.twitch.tv/kraken/channels/esl_sc2/commercial",
+        "stream_key": "https://api.twitch.tv/kraken/channels/esl_sc2/stream_key",
+        "chat": "https://api.twitch.tv/kraken/chat/esl_sc2",
+        "subscriptions": "https://api.twitch.tv/kraken/channels/esl_sc2/subscriptions",
+        "editors": "https://api.twitch.tv/kraken/channels/esl_sc2/editors",
+        "teams": "https://api.twitch.tv/kraken/channels/esl_sc2/teams",
+        "videos": "https://api.twitch.tv/kraken/channels/esl_sc2/videos"
+      }
+    },
+    "_links": {
+      "self": "https://api.twitch.tv/kraken/streams/esl_sc2",
+      "channel": "https://api.twitch.tv/kraken/channels/esl_sc2"
+    }
+  },
+  {
+    "stream": null,
+    "display_name": "noobs2ninjas",
+    "default_pic":"https://static-cdn.jtvnw.net/user-default-pictures/cd618d3e-f14d-4960-b7cf-094231b04735-profile_image-70x70.jpg",
+    "_links": {
+      "self": "https://api.twitch.tv/kraken/streams/esl_sc2",
+      "channel": "https://api.twitch.tv/kraken/channels/esl_sc2"
+    }
+  }
+];
+
+var offline=[];
+var online=[];
+
+
+categorizeChannels();
+displayChannels("all");
+
+$("#All").on("click", function() {
+	displayChannels("all");
 });
+
+$("#Online").on("click", function() {
+	displayChannels("online");
+});
+
+$("#Offline").on("click", function() {
+	displayChannels("offline");
+});
+
+function categorizeChannels() {
+	for(var i=0;i<data.length;i++) {
+		if (data[i].hasOwnProperty("display_name")) {
+			offline.push(i);
+		}
+		else {
+			online.push(i);
+		}
+	}
+};
+
+function displayChannels(mode) {
+	$(".channels").remove();
+	if (mode!=="offline") {
+		for (var i=0;i<online.length;i++) {
+			render(online[i],"online");
+		}	
+	}
+	if (mode==="online") {
+		return;
+	}
+	if (mode!=="online") {
+		for (var i=0;i<offline.length;i++) {
+			render(offline[i],"offline");
+		}	
+	}
+	
+} 
+
+function render(index,type) {
+	$("body").append('<div class="container channels" id="'+index+'"><div class="logo"></div><div class="name"></div><div class="status"></div></div>');
+
+	if (type==="online") {
+		$("#"+String(index)+" .logo").append("<img src='"+data[index].stream.logo+"'>");
+		$("#"+String(index)+" .name").append("<a href='"+data[index].stream._links.self+"'>"+data[index].stream.display_name+"</a>");
+		$("#"+String(index)+" .status").append(data[index].stream.game+":  "+data[index].stream.status);	
+	}
+	else {
+		$("#"+String(index)+" .logo").append("<img src='"+data[index].default_pic+"'>");
+		$("#"+String(index)+" .name").append("<a href='"+data[index]._links.channel+"'>"+data[index].display_name+"</a>");
+	}
+	
+}
